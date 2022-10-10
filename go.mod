b931d7d396e8fa915ffd18355d1396e4d707b1cb
m3tro1d <qazwf734@gmail.com>
2022-10-10 19:32:53 +0300

    Add user repository working with actual database

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require (
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gorilla/mux v1.8.0
+	github.com/jmoiron/sqlx v1.3.5
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/pkg/errors v0.9.1
 )
---
+++ pkg/conqueror/infrastructure/mysql/userrepository.go
@@ -0,0 +1,61 @@
+package mysql
+
+import (
+	"context"
+	"database/sql"
+
+	"conqueror/pkg/conqueror/domain"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/pkg/errors"
+)
+
+func NewUserRepository(ctx context.Context, client *sqlx.Conn) domain.UserRepository {
+	return &userRepository{
+		ctx:    ctx,
+		client: client,
+	}
+}
+
+type userRepository struct {
+	ctx    context.Context
+	client *sqlx.Conn
+}
+
+func (repo *userRepository) Store(user *domain.User) error {
+	const sqlQuery = `INSERT INTO user (id, login, password, nickname)
+		              VALUES (?, ?, ?, ?)
+		              ON DUPLICATE KEY UPDATE login=VALUES(login), password=VALUES(password),
+		                                      nickname=VALUES(nickname)`
+
+	args := []interface{}{
+		user.ID(),
+		user.Login(),
+		user.Password(),
+		user.Nickname(),
+	}
+
+	_, err := repo.client.ExecContext(repo.ctx, sqlQuery, args...)
+	return errors.WithStack(err)
+}
+
+func (repo *userRepository) GetById(id domain.UserID) (*domain.User, error) {
+	const sqlQuery = `SELECT id, login, password, nickname
+		              FROM user
+		              WHERE id = ?`
+
+	var user sqlxUser
+	err := repo.client.SelectContext(repo.ctx, &user, sqlQuery, id)
+	if err == sql.ErrNoRows {
+		return nil, errors.WithStack(domain.ErrUserNotFound)
+	} else if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	return domain.NewUser(
+		domain.UserID(user.ID),
+		user.Login,
+		user.Password,
+		user.Nickname,
+	), nil
+}
