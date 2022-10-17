2a03f5a8d95bb05f6b7f852b26bc3e8def3c17b3
m3tro1d <qazwf734@gmail.com>
2022-10-17 22:26:05 +0300

    Use typescript for client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,8 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Suck my dick, bitchezz<br />
-          I MEAN it and only it
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
