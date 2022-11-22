802150e5e2e335d771eddb8795bcd1ee60c708f3
m3tro1d <qazwf734@gmail.com>
2022-11-22 20:45:18 +0300

    Adjust request methods

--- pkg/conqueror/infrastructure/transport/router.go
+++ pkg/conqueror/infrastructure/transport/router.go
@@ -13,28 +13,28 @@ func NewRouter(publicAPI PublicAPI) http.Handler {
 	router.POST("/api/v1/user/login", handlerFunc(publicAPI.LoginUser))
 
 	router.POST("/api/v1/subject", handlerFunc(publicAPI.CreateSubject))
-	router.PUT("/api/v1/subject/:subjectID/title", handlerFunc(publicAPI.ChangeSubjectTitle))
+	router.PATCH("/api/v1/subject/:subjectID/title", handlerFunc(publicAPI.ChangeSubjectTitle))
 	router.DELETE("/api/v1/subject/:subjectID", handlerFunc(publicAPI.RemoveSubject))
 
 	router.POST("/api/v1/task", handlerFunc(publicAPI.CreateTask))
-	router.PUT("/api/v1/task/:taskID/title", handlerFunc(publicAPI.ChangeTaskTitle))
-	router.PUT("/api/v1/task/:taskID/tags", handlerFunc(publicAPI.ChangeTaskTags))
-	router.PUT("/api/v1/task/:taskID/description", handlerFunc(publicAPI.ChangeTaskDescription))
-	router.PUT("/api/v1/task/:taskID/status", handlerFunc(publicAPI.ChangeTaskStatus))
+	router.PATCH("/api/v1/task/:taskID/title", handlerFunc(publicAPI.ChangeTaskTitle))
+	router.PATCH("/api/v1/task/:taskID/tags", handlerFunc(publicAPI.ChangeTaskTags))
+	router.PATCH("/api/v1/task/:taskID/description", handlerFunc(publicAPI.ChangeTaskDescription))
+	router.PATCH("/api/v1/task/:taskID/status", handlerFunc(publicAPI.ChangeTaskStatus))
 	router.DELETE("/api/v1/task/:taskID", handlerFunc(publicAPI.RemoveTask))
 
 	router.POST("/api/v1/tasktag", handlerFunc(publicAPI.CreateTaskTag))
-	router.PUT("/api/v1/tasktag/:taskTagID/name", handlerFunc(publicAPI.ChangeTaskTagName))
+	router.PATCH("/api/v1/tasktag/:taskTagID/name", handlerFunc(publicAPI.ChangeTaskTagName))
 	router.DELETE("/api/v1/tasktag/:taskTagID", handlerFunc(publicAPI.RemoveTaskTag))
 
 	router.POST("/api/v1/note", handlerFunc(publicAPI.CreateNote))
-	router.PUT("/api/v1/note/:noteID/title", handlerFunc(publicAPI.ChangeNoteTitle))
-	router.PUT("/api/v1/note/:noteID/content", handlerFunc(publicAPI.ChangeNoteContent))
-	router.PUT("/api/v1/note/:noteID/tags", handlerFunc(publicAPI.ChangeNoteTags))
+	router.PATCH("/api/v1/note/:noteID/title", handlerFunc(publicAPI.ChangeNoteTitle))
+	router.PATCH("/api/v1/note/:noteID/content", handlerFunc(publicAPI.ChangeNoteContent))
+	router.PATCH("/api/v1/note/:noteID/tags", handlerFunc(publicAPI.ChangeNoteTags))
 	router.DELETE("/api/v1/note/:noteID", handlerFunc(publicAPI.RemoveNote))
 
 	router.POST("/api/v1/notetag", handlerFunc(publicAPI.CreateNoteTag))
-	router.PUT("/api/v1/notetag/:noteTagID/name", handlerFunc(publicAPI.ChangeNoteTagName))
+	router.PATCH("/api/v1/notetag/:noteTagID/name", handlerFunc(publicAPI.ChangeNoteTagName))
 	router.DELETE("/api/v1/notetag/:noteTagID", handlerFunc(publicAPI.RemoveNoteTag))
 
 	router.GET("/api/v1/tasks", handlerFunc(publicAPI.ListTasks))
