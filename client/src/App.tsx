55e9a4f8dddfcd9c87a067783e47775db855b34e
m3tro1d <qazwf734@gmail.com>
2022-12-22 23:54:41 +0300

    Fix positioning bullshit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import LoginPage from './components/AuthPages/LoginPage/LoginPage'
 import SignUpPage from './components/AuthPages/SignUpPage/SignUpPage'
 import Dashboard from './components/AppPages/Dashboard/Dashboard'
@@ -6,27 +6,26 @@ import TasksPage from './components/AppPages/TasksPage/TasksPage'
 import useToken from './hooks/useToken'
 import Header from './components/AppPages/common/Header/Header'
 import Navigation from './components/AppPages/common/Navigation/Navigation'
-import styles from './App.module.css'
 
 function App() {
-    const { token, setToken } = useToken()
+    const {token, setToken} = useToken()
     if (!token) {
         if (document.location.pathname == '/signup') {
-            return <SignUpPage />
+            return <SignUpPage/>
         }
 
-        return <LoginPage setToken={setToken} />
+        return <LoginPage setToken={setToken}/>
     }
 
     return (
         <div>
-            <Header />
-            <Navigation />
-            <div className={styles.mainPage}>
+            <Header/>
+            <Navigation/>
+            <div>
                 <BrowserRouter>
                     <Routes>
-                        <Route path="/" element={<Dashboard />} />
-                        <Route path="/tasks" element={<TasksPage />} />
+                        <Route path="/" element={<Dashboard/>}/>
+                        <Route path="/tasks" element={<TasksPage/>}/>
                     </Routes>
                 </BrowserRouter>
             </div>
