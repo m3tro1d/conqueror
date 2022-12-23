56ce76a3ff13bbe49425f178fa6d6c55f1e15347
m3tro1d <qazwf734@gmail.com>
2022-12-23 12:08:07 +0300

    Improve navigation

--- client/src/components/AppPages/common/Navigation/Navigation.tsx
+++ client/src/components/AppPages/common/Navigation/Navigation.tsx
@@ -5,19 +5,31 @@ function Navigation() {
     return (
         <div className={styles.navigation}>
             <ul className={styles.linksList}>
-                <li>
-                    <a href="/" title="Dashboard">
-                        <span className="material-icons">home</span>
+                <li className={styles.listItem}>
+                    <a
+                        href="/"
+                        title="Dashboard"
+                        className={styles.link}
+                    >
+                        <span className={"material-icons " + styles.icon}>home</span>
                     </a>
                 </li>
-                <li>
-                    <a href="/tasks" title="Tasks">
-                        <span className="material-icons">task</span>
+                <li className={styles.listItem}>
+                    <a
+                        href="/tasks"
+                        title="Tasks"
+                        className={styles.link}
+                    >
+                        <span className={"material-icons " + styles.icon}>task</span>
                     </a>
                 </li>
-                <li>
-                    <a href="/notes" title="Notes">
-                        <span className="material-icons">edit_note</span>
+                <li className={styles.listItem}>
+                    <a
+                        href="/notes"
+                        title="Notes"
+                        className={styles.link}
+                    >
+                        <span className={"material-icons " + styles.icon}>edit_note</span>
                     </a>
                 </li>
             </ul>
