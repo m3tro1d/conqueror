4d9314e9126d3c35dea7665f6dca3834022ecd7b
m3tro1d <qazwf734@gmail.com>
2022-12-28 01:05:53 +0300

    Add basic task form and real listing

---
+++ client/src/components/AppPages/Dashboard/TasksPanel/AddTaskForm/AddTaskForm.tsx
@@ -0,0 +1,62 @@
+import React, { FormEvent, useState } from 'react'
+import { tasksApi } from '../../../../../api/api'
+
+function AddTaskForm() {
+    const [dueDate, setDueDate] = useState('')
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault()
+        if (title === '') {
+            alert('Empty title.')
+            return
+        }
+
+        console.log(dueDate)
+
+        try {
+            await tasksApi.createTask({
+                due_date: new Date(),
+                title: title,
+                description: description,
+            })
+        } catch (error) {
+            alert('Failed to add task.')
+        }
+    }
+
+    return (
+        <form
+            onSubmit={handleSubmit}
+        >
+            <label htmlFor="due_date">Due date</label>
+            <input
+                type="date"
+                name="due_date"
+                onChange={e => setDueDate(e.target.value)}
+            />
+            <br />
+
+            <label htmlFor="title">Title</label>
+            <input
+                type="text"
+                name="title"
+                onChange={e => setTitle(e.target.value)}
+            />
+            <br />
+
+            <label htmlFor="description">Description</label>
+            <input
+                type="text"
+                name="description"
+                onChange={e => setDescription(e.target.value)}
+            />
+            <br />
+
+            <button type="submit">Add</button>
+        </form>
+    )
+}
+
+export default AddTaskForm
--- client/src/components/AppPages/TasksPage/TasksPage.tsx
+++ client/src/components/AppPages/TasksPage/TasksPage.tsx
@@ -1,53 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './TasksPage.module.css'
 import Task from '../common/Task/Task'
+import AddTaskForm from '../Dashboard/TasksPanel/AddTaskForm/AddTaskForm'
+import { tasksApi } from '../../../api/api'
 
 function TasksPage() {
-    const tasks = [
-        {
-            id: '1',
-            due_date: '2006-01-02 15:04:05.999999999 -0700 MST',
-            title: 'Listen up',
-            description: 'Here we go',
-            tags: [
-                {
-                    id: 't1',
-                    name: 'huinya',
-                },
-                {
-                    id: 't2',
-                    name: 'study',
-                },
-            ],
-            subject_id: null,
-        },
-        {
-            id: '2',
-            due_date: '2006-01-03 15:04:05.999999999 -0700 MST',
-            title: 'Its labour day and my grandpa just ate 7 fucking hot dogs',
-            description: 'Break stuff',
-            tags: [
-                {
-                    id: 't1',
-                    name: 'work',
-                },
-            ],
-            subject_id: 's1',
-        },
-        {
-            id: '3',
-            due_date: '2006-01-03 15:04:05.999999999 -0700 MST',
-            title: 'But I forgot my pen',
-            description: 'Shit the bed again',
-            tags: [],
-            subject_id: 's2',
-        },
-    ]
+    const [tasks, setTasks] = useState([])
+
+    const updateTasks = () => {
+        tasksApi
+            .listTasks()
+            .then(response => setTasks(response.tasks))
+            .catch(() => alert('Failed to fetch tasks.'))
+    }
+
+    useEffect(updateTasks, [])
 
     return (
         <div className={styles.tasksPage}>
+            <AddTaskForm />
             <ul className={styles.tasksList}>
-                {tasks.map(task => (<Task key={task.id} task={task} />))}
+                {tasks.map(task => (
+                    <Task key={task['id']} task={task} />
+                ))}
             </ul>
         </div>
     )
