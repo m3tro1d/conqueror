72b038d04381e8e3e29425fa89b7a404ad1a1155
m3tro1d <qazwf734@gmail.com>
2022-12-28 01:11:08 +0300

    Tasks fetching on tasks panel

--- client/src/components/AppPages/Dashboard/TasksPanel/TasksPanel.tsx
+++ client/src/components/AppPages/Dashboard/TasksPanel/TasksPanel.tsx
@@ -1,53 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './TasksPanel.module.css'
-import Task from "../../common/Task/Task";
+import Task from '../../common/Task/Task'
+import { tasksApi } from '../../../../api/api'
 
 function TasksPanel() {
-    // const [tasks, setTasks] = useState([])
-    // useEffect(() => {
-    //     tasksApi
-    //         .listTasks()
-    //         .then(response => setTasks(response.tasks))
-    //         .catch(() => alert('YOU FUCKED UP!!!'))
-    // }, [])
+    const [tasks, setTasks] = useState([])
 
-    const tasks = [
-        {
-            id: '1',
-            due_date: '2006-01-02 15:04:05.999999999 -0700 MST',
-            title: 'Listen up',
-            description: 'Just get the fuck up',
-            tags: [
-                {
-                    id: 't1',
-                    name: 'ood',
-                },
-                {
-                    id: 't2',
-                    name: 'study',
-                },
-            ],
-            subject_id: null,
-        },
-        {
-            id: '2',
-            due_date: '2006-01-03 15:04:05.999999999 -0700 MST',
-            title: 'Fucked up job with a fucked up paying',
-            description: 'Just get the fuck up x2',
-            tags: [
-                {
-                    id: 't1',
-                    name: 'automata',
-                },
-            ],
-            subject_id: 's1',
-        },
-    ]
+    const updateTasks = () => {
+        tasksApi
+            .listTasks()
+            .then(response => setTasks(response.tasks))
+            .catch(() => alert('Failed to fetch tasks.'))
+    }
+
+    useEffect(updateTasks, [])
 
     return (
         <div className={styles.tasksPanel}>
             <ul className={styles.tasksList}>
-                {tasks.map(task => (<Task key={task.id} task={task} />))}
+                {tasks.map(task => (
+                    <Task key={task['id']} task={task} />
+                ))}
             </ul>
         </div>
     )
