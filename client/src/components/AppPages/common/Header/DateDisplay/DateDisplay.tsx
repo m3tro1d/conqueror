35ca7de40339075982574d43ac278cab77eaa391
m3tro1d <qazwf734@gmail.com>
2023-01-05 21:44:11 +0300

    Fix date display
    Fucked in the head

--- client/src/components/AppPages/common/Header/DateDisplay/DateDisplay.tsx
+++ client/src/components/AppPages/common/Header/DateDisplay/DateDisplay.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import styles from './DateDisplay.module.css';
+import React from 'react'
+import styles from './DateDisplay.module.css'
 
 function DateDisplay(): JSX.Element {
-  return (
-    <div className={styles.dateField}>
-      {formatDate(new Date())}
-    </div>
-  );
+    return (
+        <div className={styles.dateField}>
+            {formatDate(new Date())}
+        </div>
+    )
 }
 
 function formatDate(date: Date): string {
-  const weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
-  const day = date.getDate() <= 9
-    ? '0' + date.getDate().toString()
-    : date.getDate().toString();
-  const weekDay = weekDays[date.getDay()];
-  const month = months[date.getMonth()];
+    const day = date.getDate() <= 9
+        ? '0' + date.getDate().toString()
+        : date.getDate().toString()
+    const weekDay = weekDays[date.getDay()]
+    const month = months[date.getMonth()]
 
-  return `${weekDay} ${day} ${month}`;
+    return `${weekDay} ${day} ${month}`
 }
 
-export default DateDisplay;
+export default DateDisplay
--- client/src/components/AppPages/common/Header/Header.tsx
+++ client/src/components/AppPages/common/Header/Header.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import DateDisplay from './DateDisplay/DateDisplay';
-import styles from './Header.module.css';
-import UserInfo from './UserInfo/UserInfo';
+import React from 'react'
+import DateDisplay from './DateDisplay/DateDisplay'
+import styles from './Header.module.css'
+import UserInfo from './UserInfo/UserInfo'
 
 function Header(): JSX.Element {
-  return (
-    <header className={styles.header}>
-      <div>
-        <h1 className={styles.logo}>Dashboard</h1>
-      </div>
-      <DateDisplay />
-      <UserInfo />
-    </header>
-  );
+    return (
+        <header className={styles.header}>
+            <div>
+                <h1 className={styles.logo}>Dashboard</h1>
+            </div>
+            <DateDisplay />
+            <UserInfo />
+        </header>
+    )
 }
 
-export default Header;
+export default Header
